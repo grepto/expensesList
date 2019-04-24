574d8913252d10180c51c2b2e9ca452e24ec8a9a
grepto <grepto@gmail.com>
2019-04-24 15:50:07 +0300

    refactoring result constructor in listItemUpdate

--- listItemUpdate.py
+++ listItemUpdate.py
@@ -5,11 +5,13 @@ from listItemAdd import listItemAdd
 def listItemUpdate(listId=3, itemId=3, name='Новый элемент списка', value=0, isCompensated=False):
     """"Обновляет элемент списка"""
 
-    listItemDelete(listId, itemId)
-    listItemAdd(listId, name, value, isCompensated, itemId)
+    result = listItemDelete(listId, itemId)
 
-    return
+    if result['isError'] is False:
+        result = listItemAdd(listId, name, value, isCompensated, itemId)
+
+    return result
 
 
-# listItemUpdate(3, 3, 'Органайзер для проводов IKEA', 899, True)
+# print(listItemUpdate(3, 3, 'Органайзер для проводов IKEA', 899, True))
 
