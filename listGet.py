c042cabf99611631176551e7fd78f371196b51cc
grepto <grepto@gmail.com>
2019-04-29 11:03:17 +0300

    change result return from json to dictionary

--- listGet.py
+++ listGet.py
@@ -1,10 +1,11 @@
 import json
-from _settings import listsFile, fileDir
+from _settings import listsFile, fileDir, commonResult
 
 def listGet(listId = 3):
     """"Возвращает JSON модель списка. В начале идут атрибуты списка, затем массив элементов."""
 
     fileName = fileDir + str(listId)+'.json'
+    result = commonResult
 
     with open(listsFile, 'r', encoding='utf-8') as f:
         listHead = list(filter(lambda p: p['id'] == listId, json.load(f)))
@@ -18,13 +19,14 @@ def listGet(listId = 3):
     totalValue = sum(elem['value'] for elem in listContent)
 
     if len(listHead) > 0:
-        result = dict(id=listId, name=listHead[0]['name'], totalValue=totalValue, lines=listContent)
+        resultList = dict(id=listId, name=listHead[0]['name'], totalValue=totalValue, lines=listContent)
+        result.update(resultList)
     else:
         result = dict(isError=True, errorText=f'list id {listId} does not exists')
 
-    resultJson = json.dumps(result, ensure_ascii=False, indent=2)
+    # resultJson = json.dumps(result, ensure_ascii=False, indent=2)
 
-    return resultJson
+    return result
 
 
-# print(listGet(4))
+# print(listGet(99))
