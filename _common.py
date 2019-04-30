bd8c3dcb1cd340c0393bc630f24e370d78486a85
grepto <grepto@gmail.com>
2019-04-30 15:03:02 +0300

    New common function error

--- _common.py
+++ _common.py
@@ -12,3 +12,21 @@ def newId(listData):
         return 0
 
 
+def error(errCode, *errVariables):
+    """Возвращает объект ошибки"""
+
+    errors = {
+        -1: {'template': 'not enough errVarialbes. Expected {}'},
+        1: {'template': 'list id {} does not exists', 'errVariables': 'listId'},
+        2: {'template': 'item id {} does not exists in list id {}', 'errVariables': 'itemId, listId'}
+    }
+    template = errors[errCode]['template']
+
+    try:
+        errorText = template.format(*errVariables)
+    except IndexError:
+        expectedVariables = errors[errCode]['errVariables']
+        return error(-1, expectedVariables)
+
+    return {'isError': True, 'errorText': errorText, 'errorCode': errCode}
+
